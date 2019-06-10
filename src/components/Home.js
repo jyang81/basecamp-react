c93c49337314b05fd710d04324716d2f8a52f850
jyang81 <joe.yang.design@gmail.com>
2019-06-10 09:04:00 -0700

    setup routes, rendered login page

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  // constructor() {
+  //   super()
+  //
+  // }
+
+  render() {
+    return (
+      <div className="App-header">
+      Home Page
+      </div>
+    )
+  }
+
+
+}
+
+export default Home;
