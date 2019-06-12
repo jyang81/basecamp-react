ec80bc767143a776d056218426c0c7cb6e243058
jyang81 <joe.yang.design@gmail.com>
2019-06-12 06:50:07 -0700

    removed dashboard from router, using state to render, signup/login redirects to dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 // import { withRouter } from 'react-router';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
@@ -9,30 +9,39 @@ import Signup from './components/Signup';
 
 
 class App extends Component {
-  // constructor() {
-  //   super()
-    state = {
+
+  state = {
+      loggedIn: false,
       user: {}
     }
-  // }
+
+  // static getDerivedStateFromProps(props, state) {
+  //     console.log({props}, {state})
+  //     return {user: props.user}
+  //   }
 
   setUser = (data) => {
     this.setState({
-      user: data.user
-    }, () => {console.log('in set user', this.state.user)}
-  )
-  }
+      user: data.user,
+      loggedIn: true
+    }, () => {console.log('3. set user', this.state)}
+    )}
 
   render() {
+    if (this.state.loggedIn === true && Object.keys(this.state.user).length !== 0) {
+      return <Dashboard user={this.state.user} />
+    }
     return (
       <div className="App">
         <div>BaseCamp</div>
-        {/* if not logged in, redirect to log in page */}
-
         <Router>
           <Route exact path="/login" render={() => <Login setUser={this.setUser} />} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/dashboard" render={() => Object.keys(this.state.user).length !== 0 ? <Dashboard user={this.state.user} /> : null} />
+          <Route exact path="/signup" render={() => <Signup
+              setUser={this.setUser}
+              email={this.email}
+              password={this.password}
+              /> } />
+            {console.log("4. inside router:", this.state)}
         </Router>
       </div>
     )
@@ -40,3 +49,56 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+{/*// ============= LOG IN FUNCTIONS =================
+
+  // logIn = () => {
+  //
+  //   let email = this.email.current.value
+  //   let password = this.password.current.value
+  //
+  //   fetch(URL + 'login', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({user: {email, password} })
+  //   })
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     if (data && data.jwt) {
+  //       this.saveToken(data.jwt)
+  //       this.getProfile()
+  //     } else {
+  //       const error = document.getElementById("error")
+  //       error.textContent = data.message
+  //     }
+  //   })
+  // }
+  //
+  // getProfile = () => {
+  //   let token = this.getToken()
+  //   fetch(URL + 'profile', {
+  //     headers: {
+  //       'Authorization': 'Bearer ' + token
+  //     }
+  //   })
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     this.setUser(data)
+  //   })
+  // }
+  //
+  //
+  // saveToken(jwt) {
+  //   localStorage.setItem('jwt', jwt)
+  // }
+  //
+  // getToken(jwt) {
+  //   return localStorage.getItem('jwt')
+  // }
+
+  <Route exact path="/dashboard" render={() => Object.keys(this.state.user).length !== 0 ? <Dashboard user={this.state.user} /> : null} />
+  // */}
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,23 +4,27 @@ import React, {Component} from 'react';
 class Dashboard extends Component {
   // constructor(props) {
   //   super(props)
-  //
+  //   this.state = {
+  //     loggedIn: true
+  //   }
   // }
 
-  // static getDerivedStateFromProps
 
   render() {
-    console.log("USER", this.props.user)
+    console.log("Dashboard User:", this.props.user)
     return (
       <div className="App-header">
       Dashboard Page
-
-      <p>{this.props.user.name}</p>
-      <p>{this.props.user.school}</p>
-      <p>{this.props.user.start_date}</p>
-      <p>{this.props.user.end_date}</p>
-      <p>{this.props.user.course.name}</p>
-
+      <div className="parent">
+        <p>{this.props.user.name}</p>
+        <p>{this.props.user.school}</p>
+        <p>{this.props.user.start_date}
+        {this.props.user.end_date}</p>
+        <p>{this.props.user.course.name}</p>
+        {this.props.user.to_dos.map(to_do => {
+          return <p>{to_do.item}</p>
+        })}
+      </div>
       </div>
     )
   }
